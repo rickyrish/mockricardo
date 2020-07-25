61fa0979014a77ffcc5465df2ca2b4025d08bf70
Ricardo Arana Reyes <the.ricky.rish@gmail.com>
2020-07-24 19:36:59 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.static(publicPath));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,  Accept"); // Content-Type,
+    res.header("Access-Control-Allow-Headers", "Content-Type, Origin, X-Requested-With,  Accept");
     next();
 });
 
